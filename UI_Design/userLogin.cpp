944527d4d45343717b42ad30c63d6cde382ecc33
n0miya <dulapah6@gmail.com>
2021-09-16 21:33:41 +0700

    Design some UI
    Designed some UI to give some ideas on how the program will look like.

---
+++ UI_Design/userLogin.cpp
@@ -0,0 +1,9 @@
+// If user select "user login"
+
+#include <stdio.h>
+
+int main() {
+    printf("Please enter your credentials (type /back to go back):\n");
+    printf("Username: \n");
+    printf("Password: \n");
+}

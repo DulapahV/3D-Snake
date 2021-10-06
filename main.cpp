d66e12993a2d9672d4d614d24f48fbee68324b31
natheetarn <natheetarn@gmail.com>
2021-10-06 14:24:26 +0700

    Add_book() function
    - implemented add_book() function
    - changed struct book form int id to char
    - created ID_LIMIT, NAME_LIMIT for easy adjustment
    - added prototype functions for deleting books and adjusting stock

--- main.cpp
+++ main.cpp
@@ -5,8 +5,10 @@
 
 using namespace std;
 
+#define ID_LIMIT 10
+#define NAME_LIMIT 30
 struct Book{
-    int id;
+    char id[10];
     char name[30];
     char author[30];
     int pub_date[3];// [dd,mm,yyyy]
@@ -29,9 +31,17 @@ struct user{
 void display_front_page();
 void display_login_pg();
 void display_register_pg();
+void display_admin_pg();
+
 void clrscr();
 int check_credential(char username, char password);
 int get_choice(int minval, int maxval);
+
+int add_book();
+int delete_book();
+int set_stock();
+int add_stock();
+int book_id_checker(char* id);
 
 int main(){
     display_front_page();
@@ -122,6 +132,40 @@ int get_choice(int minval, int maxval){// pass min,max to check if user input is
     return 0;
 }
 
+int add_book(){
+    struct Book new_book;
+    printf("[Add Book] Enter book info: \n");
+    // while(1){
+    printf("ID: ");
+    fgets(new_book.id,ID_LIMIT,stdin);
+    // if(book_id_checker(new_book.id)!=-1)
+    //     break;
+    // else{
+    //     printf("Id error(Redundant) please try again\n");
+    // }
+    // }
+    printf("Book name: ");
+    fgets(new_book.name,NAME_LIMIT,stdin);
+    printf("Author name: ");
+    fgets(new_book.author,NAME_LIMIT,stdin);
+    printf("Published Date(DD): ");
+    fflush(stdin);
+    scanf("%d",&new_book.pub_date[0]);
+    printf("Published Date(MM): ");
+    fflush(stdin);
+    scanf("%d",&new_book.pub_date[1]);
+    printf("Published Date(YYYY): ");
+    fflush(stdin);
+    scanf("%d",&new_book.pub_date[2]);
+    printf("Price($): ");
+    scanf("%f",&new_book.price);
+    printf("Genre(0-10): ");
+    scanf("%d",&new_book.genre);
+    printf("Initial Stock: ");
+    scanf("%d",&new_book.stock);
+
+    books.push_back(new_book);
+}
 void clrscr(){
     system("cls");
 }

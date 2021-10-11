9ceef27c2c0c7362c0d457a3db640c388d4bc948
bothsupernova <90545201+bothsupernova@users.noreply.github.com>
2021-10-11 21:56:44 +0700

    Add files via upload

---
+++ C_project_Menu.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+void main()
+{
+    int a;
+    printf("Select Menu\n1.Search book\n2.Buy Book\n3.Show book list\n4.Check Out");
+    scanf("%d",&a);
+
+    switch (a)
+    {
+        case 1 :
+        //search book function
+
+            break;
+
+         case 2 :
+            //Buy book function
+
+            break;
+
+         case 3 :
+            //show book function
+
+            break;
+         case 4 :
+            //Check out function
+
+            break;
+
+    }
+}
